d415767144af3d2a7f412e53edc7b1f73d566bfb
medkill007 <plaszlo007@gmail.com>
2020-08-29 13:39:31 +0200

    feltölt

---
+++ live_eye_detect/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace live_eye_detect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(168, 47);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 44);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(16, 80);
+            this.pic.Margin = new System.Windows.Forms.Padding(4);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(1727, 798);
+            this.pic.TabIndex = 2;
+            this.pic.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Eszköz: ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(478, 44);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(654, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 26);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "test";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(651, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Exel file name";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 846);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ live_eye_detect/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accord.Video.FFMPEG;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace live_eye_detect
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevicesList;
+        private IVideoSource videoSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // get list of video devices
+            videoDevicesList = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo videoDevice in videoDevicesList)
+                comboBox1.Items.Add(videoDevice.Name);
+            if (comboBox1.Items.Count > 0)            
+                comboBox1.SelectedIndex = 0;
+            else
+                MessageBox.Show("No video sources found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            // stop the camera on window close
+            this.Closing += Form1_Closing;
+        }
+
+        private void Form1_Closing(object sender, CancelEventArgs e)
+        {
+            // signal to stop
+            if (videoSource != null && videoSource.IsRunning)
+                videoSource.SignalToStop();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            csv.AppendLine("X;Y;Size;\n");
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            videoSource = new VideoCaptureDevice(videoDevicesList[comboBox1.SelectedIndex].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(Device_NewFrame);  //camera_NewFrame simán camera   --- Device_NewFrame szemfelismeréssel camera
+            videoSource.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //videoSource.NewFrame += new NewFrameEventHandler(Device_NewFrame);
+            videoSource.SignalToStop();
+            if (videoSource != null && videoSource.IsRunning && pic.Image != null)
+            {
+                pic.Image.Dispose();
+            }
+        }
+
+        private void camera_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            pic.Image = bitmap;
+        }
+
+        public StringBuilder csv = new StringBuilder();
+        public void csv_file(int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+            {
+                csv.Append(x[i] + ";");
+            }
+            csv.Append("\n");
+
+            File.WriteAllText(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\" + textBox2.Text+".csv", csv.ToString());
+        }
+
+
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascades\\haarcascade_eye.xml");
+        private void Device_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            Rectangle[] rectangles = cascadeClassifier.DetectMultiScale(grayImage, 3.5, 3);
+            foreach (Rectangle rectangle in rectangles)
+            {
+                string result = String.Concat(rectangle);
+                //Console.WriteLine(result);  {X=671,Y=254,Width=70,Height=70}
+                csv_file(eyes_coordinate(result));
+
+
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Pen pen = new Pen(Color.Red, 2))
+                    {
+                        graphics.DrawEllipse(pen, rectangle);
+                    }
+                }
+            }
+            pic.Image = bitmap;
+        }
+
+
+        private int[] eyes_coordinate(string x)
+        {
+            int[] coordinate = new int[3];
+            string num = "";
+            for (int i = 1; i < x.Length; i++)
+            {
+                if (x[i - 1] == 'X' && x[i] == '=')
+                {
+                    num = "";
+                    for (int o = i + 1; o < x.Length; o++)
+                    {
+                        if (x[o] != ',')
+                        {
+                            num += x[o];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    coordinate[0] = Int32.Parse(num);
+                }
+                if (x[i - 1] == 'Y' && x[i] == '=')
+                {
+                    num = "";
+                    for (int o = i + 1; o < x.Length; o++)
+                    {
+                        if (x[o] != ',')
+                        {
+                            num += x[o];
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    coordinate[1] = Int32.Parse(num);
+                }
+                if (i >= 6) {
+                    if (x[i - 5] == 'W' && x[i] == '=')
+                    {
+                        num = "";
+                        for (int o = i + 1; o < x.Length; o++)
+                        {
+                            if (x[o] != ',')
+                            {
+                                num += x[o];
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        coordinate[2] = Int32.Parse(num);
+                    }
+                }
+            }
+            return coordinate;
+        }
+
+
+    }
+}
